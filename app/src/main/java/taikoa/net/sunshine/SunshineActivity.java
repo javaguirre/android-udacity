899b5371bebd5ef88cf4fac01cf0f1d8895886bf
Javier Aguirre <contacto@javaguirre.net>
2014-08-28 17:43:46 +0200

    Moving Fragment to own file, adding AsyncTask for FetchWeather

--- app/src/main/java/taikoa/net/sunshine/SunshineActivity.java
+++ app/src/main/java/taikoa/net/sunshine/SunshineActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.app.ActionBar;
 import android.app.Fragment;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -13,6 +14,12 @@ import android.os.Build;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -26,7 +33,7 @@ public class SunshineActivity extends Activity {
         setContentView(R.layout.activity_sunshine);
         if (savedInstanceState == null) {
             getFragmentManager().beginTransaction()
-                    .add(R.id.container, new PlaceholderFragment())
+                    .add(R.id.container, new ForecastFragment())
                     .commit();
         }
     }
@@ -49,46 +56,5 @@ public class SunshineActivity extends Activity {
             return true;
         }
         return super.onOptionsItemSelected(item);
-    }
-
-    /**
-     * A placeholder fragment containing a simple view.
-     */
-    public static class PlaceholderFragment extends Fragment {
-
-        public PlaceholderFragment() {
-        }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_sunshine, container, false);
-
-            String[] forecastArray = {
-                    "Today",
-                    "Tomorrow",
-                    "Weds",
-                    "Thurs"
-            };
-
-            List<String> weekForecast = new ArrayList<String>(
-                    Arrays.asList(forecastArray)
-            );
-
-            ArrayAdapter<String> mForecastAdapter = new ArrayAdapter<String>(
-                    // The current context (this fragment's parent activity)
-                    getActivity(),
-                    // ID of list item layout
-                    R.layout.list_item_forecast,
-                    // ID of textview to populate
-                    R.id.list_item_forecast_textview,
-                    weekForecast
-            );
-
-            ListView mView = (ListView) rootView.findViewById(R.id.listview_forecast);
-            mView.setAdapter(mForecastAdapter);
-
-            return rootView;
-        }
     }
 }
